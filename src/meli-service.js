d4d3ba4c3c483ca337e1f2796479cc91b47250c3
Pablo Felitti <pablofelitti@Pablos-MBP-15.fibertel.com.ar>
2022-05-26 23:54:03 -0300

    added try finally

--- src/meli-service.js
+++ src/meli-service.js
@@ -22,78 +22,82 @@ function unifyId(id) {
 const retrieveCheapFullProducts = async function () {
 
     const client = await meliDaoDb.getConnection()
+    try {
+        let categories = await meliDaoRest.getCategories()
 
-    let categories = await meliDaoRest.getCategories()
+        console.log(categories.length + ' categories retrieved');
 
-    console.log(categories.length + ' categories retrieved');
+        let allCategoriesPublicationPromises = categories.map(category => {
+            return meliDaoRest.getPublicationsWithFilters(category, client);
+        });
 
-    let allCategoriesPublicationPromises = categories.map(category => {
-        return meliDaoRest.getPublicationsWithFilters(category, client);
-    });
+        let blacklist = await meliDaoDb.loadBlacklist(client)
 
-    let blacklist = await meliDaoDb.loadBlacklist(client)
+        let publications = await Promise.all(allCategoriesPublicationPromises)
+        publications = publications.reduce((prev, curr) => curr.concat(prev));
 
-    let publications = await Promise.all(allCategoriesPublicationPromises)
-    publications = publications.reduce((prev, curr) => curr.concat(prev));
+        console.log('Received these publications from Mercado Libre:')
+        console.log(JSON.stringify(publications.map(it => unifyId(it.id))))
 
-    console.log('Received these publications from Mercado Libre:')
-    console.log(JSON.stringify(publications.map(it => unifyId(it.id))))
+        console.log('Blacklisted items found:')
+        console.log(JSON.stringify(publications.map(it => unifyId(it.id)).filter(publicationId => blacklist.map(blacklistItem => blacklistItem.id).includes(publicationId))))
 
-    console.log('Blacklisted items found:')
-    console.log(JSON.stringify(publications.map(it => unifyId(it.id)).filter(publicationId => blacklist.map(blacklistItem => blacklistItem.id).includes(publicationId))))
+        publications = publications.filter(publication => !blacklist.map(blacklistItem => blacklistItem.id).includes(unifyId(publication.id)))
 
-    publications = publications.filter(publication => !blacklist.map(blacklistItem => blacklistItem.id).includes(unifyId(publication.id)))
+        let alreadyNotifiedPublications = await meliDaoDb.loadAlreadyNotifiedPublications(publications.map(it => unifyId(it.id)), client)
 
-    let alreadyNotifiedPublications = await meliDaoDb.loadAlreadyNotifiedPublications(publications.map(it => unifyId(it.id)), client)
+        let publicationsToUpdate = []
+        let currentDatetime = dateUtils.currentDate();
+        let alreadyNotifiedPublicationResults = alreadyNotifiedPublications[0]
+            .filter(it => {
+                let Difference_In_Time = Math.abs(new Date(it.notified_date).getTime() - currentDatetime.getTime())
+                let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24)
+                if (Difference_In_Days >= 10) {
+                    publicationsToUpdate.push(it)
+                    return false
+                }
+                return true
+            })
+        const alreadyNotifiedPublicationsIds = alreadyNotifiedPublicationResults
+            .map(it => it.id)
 
-    let publicationsToUpdate = []
-    let currentDatetime = dateUtils.currentDate();
-    let alreadyNotifiedPublicationResults = alreadyNotifiedPublications[0]
-        .filter(it => {
-            let Difference_In_Time = Math.abs(new Date(it.notified_date).getTime() - currentDatetime.getTime())
-            let Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24)
-            if (Difference_In_Days >= 10) {
-                publicationsToUpdate.push(it)
-                return false
+        console.log('Publications already sent:')
+        console.log(JSON.stringify(alreadyNotifiedPublicationsIds))
+
+        let publicationsReadyToNotify = publications
+            .filter(it => !alreadyNotifiedPublicationsIds.includes(unifyId(it.id)))
+
+        publicationsReadyToNotify = unique(publicationsReadyToNotify)
+
+        if (publicationsReadyToNotify.length !== 0) {
+
+            console.log('Publications ready to be notified:');
+            console.log(publicationsReadyToNotify.map(it => unifyId(it.id)));
+
+            for (let pub of publicationsReadyToNotify) {
+                await sendQueue(createNotificationMessage(pub))
             }
-            return true
-        })
-    const alreadyNotifiedPublicationsIds = alreadyNotifiedPublicationResults
-        .map(it => it.id)
 
-    console.log('Publications already sent:')
-    console.log(JSON.stringify(alreadyNotifiedPublicationsIds))
+            publicationsReadyToNotify = publicationsReadyToNotify.filter(it => !publicationsToUpdate.map(it2 => it2.id).includes(unifyId(it.id)))
 
-    let publicationsReadyToNotify = publications
-        .filter(it => !alreadyNotifiedPublicationsIds.includes(unifyId(it.id)))
+            if (publicationsReadyToNotify.length > 0) {
+                let newPublicationsNotified = await meliDaoDb.saveNotifiedPublication(publicationsReadyToNotify.map(it => [unifyId(it.id), it.title, it.price, currentDatetime]), client)
 
-    publicationsReadyToNotify = unique(publicationsReadyToNotify)
-
-    if (publicationsReadyToNotify.length !== 0) {
-
-        console.log('Publications ready to be notified:');
-        console.log(publicationsReadyToNotify.map(it => unifyId(it.id)));
-
-        for (let pub of publicationsReadyToNotify) {
-            await sendQueue(createNotificationMessage(pub))
-        }
-
-        publicationsReadyToNotify = publicationsReadyToNotify.filter(it => !publicationsToUpdate.map(it2 => it2.id).includes(unifyId(it.id)))
-
-        if (publicationsReadyToNotify.length > 0) {
-            let newPublicationsNotified = await meliDaoDb.saveNotifiedPublication(publicationsReadyToNotify.map(it => [unifyId(it.id), it.title, it.price, currentDatetime]), client)
-
-            if (publicationsToUpdate.length === 0) {
-                return newPublicationsNotified
+                if (publicationsToUpdate.length === 0) {
+                    return newPublicationsNotified
+                } else {
+                    let oldPublicationsUpdated = await updateNotifiedPublications(publicationsToUpdate, currentDatetime, client)
+                    return [newPublicationsNotified, oldPublicationsUpdated]
+                }
             } else {
-                let oldPublicationsUpdated = await updateNotifiedPublications(publicationsToUpdate, currentDatetime, client)
-                return [newPublicationsNotified, oldPublicationsUpdated]
+                return await updateNotifiedPublications(publicationsToUpdate, currentDatetime, client)
             }
         } else {
-            return await updateNotifiedPublications(publicationsToUpdate, currentDatetime, client)
+            console.log('No new publications to notify');
         }
-    } else {
-        console.log('No new publications to notify');
+    } finally {
+        console.log('Closing connection')
+        await client.end()
     }
 }
 
